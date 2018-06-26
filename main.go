3f749938343763f6602bca2a21a7ab1764d2195b
Kpovoc <kpovoc@protonmail.com>
2018-06-25 23:06:52 -0500

    Issue #7 - "Add First Run Script"
    - Added a check for the main-conf.json file, and a user selection to
      start the setup wizard.
    - Fleshed out rough Setup Wizard
    
    Signed-off-by: Kpovoc <kpovoc@protonmail.com>

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"gitlab.com/Kpovoc/JBot-Go/plugin"
 	"log"
 	"gitlab.com/Kpovoc/JBot-Go/web"
+	"fmt"
 )
 
 type MainConf struct {
@@ -31,17 +32,13 @@ func main() {
 	resourceDir := *rd
 	userDataDir := *udd
 
-	rand.Seed(time.Now().UTC().UnixNano()) // Go ahead and seed rand for plugins that need it.
-	data, err := ioutil.ReadFile(userDataDir + "/main-conf.json")
-	if err != nil {
-		panic(err)
-	}
+	// Go ahead and seed rand for plugins that need it.
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	var conf MainConf
-	if err = json.Unmarshal(data, &conf); err != nil {
-		panic(err)
-	}
+	// Initialize the application config from file.
+	conf := readInitialConfig(userDataDir)
 
+	// Initialize application plugins
 	plugin.InitPlugins(resourceDir)
 
 	// TODO: Implement with buffer channel so some parts can fail while others remain
@@ -50,7 +47,7 @@ func main() {
 	go ircbot.Start(conf.IRC, fatalChan)
 	go web.Start(resourceDir + "/web")
 
-	err = <- fatalChan
+	err := <- fatalChan
 	if nil != err {
 		log.Printf("Exited with error: %s\n", err)
 	} else {
@@ -62,4 +59,212 @@ func main() {
 	//	fmt.Printf("An Error has occured:\nMsg: %s\nErr: %s\n", errMsg, err)
 	//	return
 	//}
+}
+
+func ExitWithError(errMsg string) {
+	log.Fatalf("An error has occurred:\n  %s\n  Application will now exit.", errMsg)
+}
+
+func readInitialConfig(userDataDir string) MainConf {
+	var conf MainConf
+
+	data, err := ioutil.ReadFile(userDataDir + "/main-conf.json")
+	if nil != err {
+		// Either error with current main-conf.json file, or file does not exist.
+		askUserToStartConfigWizard(userDataDir)
+
+		data, err = ioutil.ReadFile(userDataDir + "/main-conf.json")
+		if nil != err {
+			ExitWithError(err.Error())
+		}
+	}
+
+	if err = json.Unmarshal(data, &conf); err != nil {
+		ExitWithError(err.Error())
+	}
+
+	return conf
+}
+
+func askUserToStartConfigWizard(userDataDir string) {
+	userChoice := "y"
+	fmt.Printf(
+		"Could not read %s/main-conf.json. " +
+			"Would you like to initiate the setup wizard? (y/n) [%s]: ", userDataDir, userChoice)
+	fmt.Scanf("%s", &userChoice)
+
+	for "y" != userChoice && "n" != userChoice {
+		fmt.Printf("Unrecognized choice: %s\n", userChoice)
+		fmt.Printf("Would you like to initiate the setup wizard? (y/n) [%s]: ", userChoice)
+		fmt.Scanf("%s", &userChoice)
+	}
+
+	if ("n" == userChoice) {
+		log.Fatalln(
+			"ChatSteward can not operate without a valid main-conf.json. " +
+				"Application will now exit.")
+	}
+
+	createConfig(userDataDir)
+}
+
+func createConfig(userDataDir string) {
+	mainConfStart :=
+		"{\n"
+	discordConf :=
+		"  \"Discord\": {\n" +
+		"    \"BotToken\": \"\"\n" +
+		"  },\n"
+	ircConfStart :=
+		"  \"IRC\": {\n" +
+		"    \"Servers\": [\n"
+	ircServerConfStart :=
+		"      {\n"
+	ircServerConfServer :=
+		"        \"Server\": "
+	ircServerConfPort :=
+		"        \"Port\": "
+	ircServerConfNick :=
+		"        \"Nick\": "
+	ircServerConfUser :=
+		"        \"User\": "
+	ircServerConfChannelsStart :=
+		"        \"Channels\": [\n"
+	ircServerConfChannelsEnd :=
+		"        ]\n"
+	ircServerConfEndComma :=
+		"      },\n"
+	ircServerConfEnd :=
+		"      }\n"
+	ircConfEnd :=
+		"    ]\n" +
+		"  }\n"
+	mainConfEnd :=
+		"}"
+
+	out := mainConfStart +
+		discordConf +
+		ircConfStart +
+		ircServerConfStart +
+		ircServerConfServer
+
+	server := ""
+	port := ""
+	nick := ""
+	user := ""
+	channel := ""
+	fmt.Println("Enter IRC Server Information:")
+
+	fmt.Print("Server (Ex: irc.geekshed.net): ")
+	fmt.Scanf("%s", &server)
+	out = out + "\"" + server + "\",\n" +
+		ircServerConfPort
+	server = ""
+
+	fmt.Print("Port (Ex: 6667): ")
+	fmt.Scanf("%s", &port)
+	out = out + "\"" + port + "\",\n" +
+		ircServerConfNick
+	port = ""
+
+	fmt.Print("Nick (Ex: Stuart): ")
+	fmt.Scanf("%s", &nick)
+	out = out + "\"" + nick + "\",\n" +
+		ircServerConfUser
+	nick = ""
+
+	fmt.Print("User (Ex: stuart): ")
+	fmt.Scanf("%s", &user)
+	out = out + "\"" + user + "\",\n" +
+		ircServerConfChannelsStart
+	user = ""
+
+	fmt.Print("Channel Name (Ex: #jupitercolony): ")
+	fmt.Scanf("%s", &channel)
+	out = out +
+		"          \"" + channel + "\""
+	channel = ""
+
+	userAddChannel := "y"
+	fmt.Printf("Add another channel for this Server? (y/n) [%s]: ", userAddChannel)
+	fmt.Scanf("%s", &userAddChannel)
+
+	for "y" == userAddChannel {
+		out = out + ",\n"
+		fmt.Print("Channel Name (Ex: #jupitercolony): ")
+		fmt.Scanf("%s", &channel)
+		out = out +
+			"          \"" + channel + "\""
+		channel = ""
+
+		fmt.Printf("Add another channel for this Server? (y/n) [%s]: ", userAddChannel)
+		fmt.Scanf("%s", &userAddChannel)
+	}
+	out = out + "\n" +
+		ircServerConfChannelsEnd
+
+	userAddNewServerConf := "y"
+	fmt.Printf("Add new IRC Server? (y/n) [%s]: ", userAddNewServerConf)
+	fmt.Scanf("%s", &userAddNewServerConf)
+
+	for "y" == userAddNewServerConf {
+		out = out +
+			ircServerConfEndComma +
+			ircServerConfStart +
+			ircServerConfServer
+
+		fmt.Print("Server (Ex: irc.geekshed.net): ")
+		fmt.Scanf("%s", &server)
+		out = out + "\"" + server + "\",\n" +
+			ircServerConfPort
+		server = ""
+
+		fmt.Print("Port (Ex: 6667): ")
+		fmt.Scanf("%s", &port)
+		out = out + "\"" + port + "\",\n" +
+			ircServerConfNick
+		port = ""
+
+		fmt.Print("Nick (Ex: Stuart): ")
+		fmt.Scanf("%s", &nick)
+		out = out + "\"" + nick + "\",\n" +
+			ircServerConfUser
+		nick = ""
+
+		fmt.Print("User (Ex: stuart): ")
+		fmt.Scanf("%s", &user)
+		out = out + "\"" + user + "\",\n" +
+			ircServerConfChannelsStart
+		user = ""
+
+		fmt.Print("Channel Name (Ex: #jupitercolony): ")
+		fmt.Scanf("%s", &channel)
+		out = out +
+			"          \"" + channel + "\""
+		channel = ""
+
+		userAddChannel := "y"
+		fmt.Printf("Add another channel for this Server? (y/n) [%s]: ", userAddChannel)
+		fmt.Scanf("%s", &userAddChannel)
+
+		for "y" == userAddChannel {
+			out = out + ",\n"
+			fmt.Print("Channel Name (Ex: #jupitercolony): ")
+			fmt.Scanf("%s", &channel)
+			out = out +
+				"          \"" + channel + "\""
+			channel = ""
+
+			fmt.Printf("Add another channel for this Server? (y/n) [%s]: ", userAddChannel)
+			fmt.Scanf("%s", &userAddChannel)
+		}
+		out = out + "\n" +
+			ircServerConfChannelsEnd
+	}
+	out = out +
+		ircServerConfEnd +
+		ircConfEnd +
+		mainConfEnd
+
+	_ = ioutil.WriteFile(userDataDir + "/main-conf.json", []byte(out), 0644)
 }
